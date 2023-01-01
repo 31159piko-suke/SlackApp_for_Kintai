a2a7118ba0143114ad5d899c63d5c033f2957554
Kosuke Takahashi <61932642+31159piko-suke@users.noreply.github.com>
2023-01-01 21:51:59 +0900

    update

---
+++ src/lambda_function.py
@@ -0,0 +1,183 @@
+from ast import Dict
+import os
+import re
+import logging
+from datetime import datetime, timezone, timedelta
+from typing import List
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+from slack_bolt.response.response import BoltResponse
+
+import notion as notion
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+)
+
+t_delta = timedelta(hours=5)  # 日本時間朝4:00に日付更新
+JST = timezone(t_delta, "JST")
+MONTH = {
+    "1": "JAN",
+    "2": "FEB",
+    "3": "MAR",
+    "4": "APR",
+    "5": "MAY",
+    "6": "JUN",
+    "7": "JUL",
+    "8": "AUG",
+    "9": "SEP",
+    "10": "OCT",
+    "11": "NOV",
+    "12": "DEC",
+}
+
+database_id = os.environ.get(MONTH[str(datetime.now(JST).month)])
+
+
+def lambda_handler(event, context):
+    slack_handler = SlackRequestHandler(app=app)
+    return slack_handler.handle(event, context)
+
+
+@app.use
+def no_retry(context, next):
+    if context.get("lambda_request", {}).get("headers", {}).get("x-slack-retry-num", False):
+        return BoltResponse(status=200, body="no need retry")
+    else:
+        next()
+
+
+def push_button(say, body, logger):
+    logger.info(body)
+    channel: str = body["event"]["channel"]
+
+    say(
+        channel=channel,
+        text="yahho-",
+        blocks=[
+            {
+                "type": "section",
+                "block_id": "new",
+                "text": {"type": "mrkdwn", "text": "おつおつ"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "出勤", "emoji": True},
+                    "value": "syukkin",
+                    "action_id": "button_syukkin-action",
+                },
+            }
+        ],
+    )
+
+    date: str = f"{datetime.now(JST).day}日"
+    results: Dict[str, str] = notion.get_pages_from_database(database_id)
+    titles: List[str] = [result["properties"]["Name"]["title"][0]["plain_text"] for result in results]
+
+    if date not in titles:
+        notion.create_page(database_id=database_id, title=date)
+    else:
+        logger.warning("Todays record is alredy existed.")
+
+
+def syukkin_button(body, client, logger):
+    logger.info(body)
+    channel: str = body["channel"]["id"]
+    ts: str = body["message"]["ts"]
+
+    syukkin_time: str = datetime.now(JST).strftime("%m/%d %H:%M")
+
+    client.chat_postMessage(channel=channel, thread_ts=ts, text=f"[出勤] {syukkin_time}")
+
+    client.chat_update(
+        channel=channel,
+        ts=ts,
+        text="yahho-",
+        blocks=[
+            {
+                "type": "section",
+                "block_id": syukkin_time,
+                "text": {"type": "mrkdwn", "text": "おつおつ"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "退勤", "emoji": True},
+                    "value": "taikin",
+                    "action_id": "button_taikin-action",
+                },
+            }
+        ],
+    )
+
+
+def taikin_button(body, client, logger):
+    logger.info(body)
+    channel: str = body["channel"]["id"]
+    ts: str = body["message"]["ts"]
+
+    syukkin_time: datetime = datetime.strptime(body["actions"][0]["block_id"], "%m/%d %H:%M")
+    taikin_time: datetime = datetime.strptime(datetime.now(JST).strftime("%m/%d %H:%M"), "%m/%d %H:%M")
+    kinmu_time: int = (taikin_time - syukkin_time).seconds
+
+    client.chat_postMessage(channel=channel, thread_ts=ts, text=f"[退勤] {taikin_time.strftime('%m/%d %H:%M')}")
+
+    client.chat_update(
+        channel=channel,
+        ts=ts,
+        text="yahho-",
+        blocks=[
+            {
+                "type": "section",
+                "block_id": "taikin",
+                "text": {"type": "mrkdwn", "text": "おつおつ"},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "出勤", "emoji": True},
+                    "value": "syukkin",
+                    "action_id": "button_syukkin-action",
+                },
+            }
+        ],
+    )
+
+    date: str = f"{datetime.now(JST).day}日"
+    results: Dict = notion.get_pages_from_database(database_id)
+    ids: Dict[str:str] = {result["properties"]["Name"]["title"][0]["plain_text"]: result["id"] for result in results}
+    notion.update_time_property(ids[date], kinmu_time // 60)
+
+
+def message(body, message, logger):
+    logger.info(body)
+    date: str = f"{datetime.now(JST).day}日"
+    results: Dict = notion.get_pages_from_database(database_id)
+    ids: Dict[str:str] = {result["properties"]["Name"]["title"][0]["plain_text"]: result["id"] for result in results}
+
+    tags = []
+    texts = []
+    temp_test = []
+    for i in message["text"].split("\n"):
+        if i.startswith("[") and i.endswith("]"):
+            tags.append(i[1:-1])
+            if temp_test:
+                texts.append("\n".join(temp_test))
+                temp_test = []
+        else:
+            temp_test.append(i)
+    else:
+        texts.append("\n".join(temp_test))
+
+    progress: Dict[str, str] = {k: v for k, v in zip(tags, texts)}
+
+    notion.update_tags_property(ids[date], tags)
+    notion.add_progress(ids[date], progress)
+
+
+def arc_func(ack):
+    ack()
+
+
+app.event("app_mention")(ack=arc_func, lazy=[push_button])
+app.action("button_syukkin-action")(ack=arc_func, lazy=[syukkin_button])
+app.action("button_taikin-action")(ack=arc_func, lazy=[taikin_button])
+app.message(re.compile(r"^[[]"))(ack=arc_func, lazy=[message])
---
+++ src/notion.py
@@ -0,0 +1,159 @@
+from datetime import datetime
+import json
+import os
+from typing import Dict, List
+import logging
+import requests
+
+API_SECRET = os.environ.get("API_SECRET")
+HEADERS = {
+    "Authorization": f"Bearer {API_SECRET}",
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+
+def get_pages_from_database(database_id: str) -> Dict:
+    res = requests.post(
+        url=f"https://api.notion.com/v1/databases/{database_id}/query",
+        headers=HEADERS,
+    )
+    if res.status_code == 200:
+        logging.info("get_pages_from_database: success!")
+        return res.json()["results"]
+    else:
+        logging.error(f"get_pages_from_database: {res.json()}")
+        raise Exception("Failed to get pages info from database.")
+
+
+def create_page(
+    database_id: str,
+    tags: List[str] = [],
+    title: str = "Title",
+) -> None:
+    body = {
+        "parent": {"database_id": database_id},
+        "icon": {"emoji": "🚀"},
+        "cover": {
+            "external": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Abra%C3%A7ando_as_Estrelas.jpg/1024px-Abra%C3%A7ando_as_Estrelas.jpg"
+            }
+        },
+        "properties": {
+            "Name": {"title": [{"text": {"content": title}}]},
+            "Tags": {"multi_select": [{"name": i} for i in tags]},
+            "Time": {"number": 0},
+        },
+    }
+    res = requests.post(
+        url=f"https://api.notion.com/v1/pages",
+        data=json.dumps(body),
+        headers=HEADERS,
+    )
+    if res.status_code == 200:
+        logging.info("create_page: success!")
+    else:
+        logging.error(f"create_page: {res.json()}")
+        raise Exception("Failed to create page.")
+
+
+def add_progress(
+    page_id: str,
+    progress: Dict[str, str],
+) -> None:
+    def _contents(tag: str, progress: str) -> List[Dict]:
+        return [
+            {
+                "object": "block",
+                "type": "heading_3",
+                "heading_3": {"rich_text": [{"type": "text", "text": {"content": tag}}]},
+            },
+            {
+                "object": "block",
+                "type": "quote",
+                "quote": {"rich_text": [{"type": "text", "text": {"content": progress}}]},
+            },
+        ]
+
+    body = {
+        "children": sum([_contents(k, v) for k, v in progress.items()], []),
+    }
+
+    res = requests.patch(
+        url=f"https://api.notion.com/v1/blocks/{page_id}/children",
+        data=json.dumps(body),
+        headers=HEADERS,
+    )
+    if res.status_code == 200:
+        logging.info("add_progress: success!")
+    else:
+        logging.error(f"add_progress: {res.json()}")
+        raise Exception("Failed to update page.")
+
+
+def update_tags_property(
+    page_id: str,
+    tags=List[str],
+) -> None:
+    def _get_tags_property(page_id: str) -> List[str]:
+        res = requests.get(
+            url=f"https://api.notion.com/v1/pages/{page_id}/properties/Tags",
+            headers=HEADERS,
+        )
+        if res.status_code == 200:
+            logging.info("get_tags_property: success!")
+            return [r["name"] for r in res.json()["multi_select"]]
+        else:
+            logging.error(f"get_tags_property: {res.json()}")
+            raise Exception("Not found the page.")
+
+    exsisted_tags: List[str] = _get_tags_property(page_id)
+
+    body = {
+        "properties": {
+            "Tags": {"multi_select": [{"name": i} for i in exsisted_tags + tags]},
+        }
+    }
+    res = requests.patch(
+        url=f"https://api.notion.com/v1/pages/{page_id}",
+        data=json.dumps(body),
+        headers=HEADERS,
+    )
+    if res.status_code == 200:
+        logging.info("update_page: success!")
+    else:
+        logging.error(f"update_tags_property: {res.json()}")
+        raise Exception("Failed to add tags property.")
+
+
+def update_time_property(
+    page_id: str,
+    time: int,
+) -> None:
+    def _get_time_property(page_id: str) -> int:
+        res = requests.get(
+            url=f"https://api.notion.com/v1/pages/{page_id}/properties/Time",
+            headers=HEADERS,
+        )
+        if res.status_code == 200:
+            return res.json()["number"]
+        else:
+            raise Exception("Not found the page.")
+
+    cumulative_time: int = _get_time_property(page_id)
+
+    body = {
+        "properties": {
+            "Time": {"number": cumulative_time + time},
+        }
+    }
+    res = requests.patch(
+        url=f"https://api.notion.com/v1/pages/{page_id}",
+        data=json.dumps(body),
+        headers=HEADERS,
+    )
+    if res.status_code == 200:
+        logging.info("update_property: success!")
+    else:
+        logging.error(f"update_tags_property: {res.json()}")
+        raise Exception("Failed to add time property.")
